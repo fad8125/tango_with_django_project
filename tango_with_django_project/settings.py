ef5d5d396ffdd33461cfb43ca216c489f4f86e7c
Muhammad Fahd Asif <2262555a@student.gla.ac.uk>
2018-02-09 18:09:17 +0000

    Chapter 9 Done

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -85,6 +85,11 @@ DATABASES = {
     }
 }
 
+PASSWORD_HASHERS = (
+     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+ )
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -92,6 +97,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
@@ -103,6 +109,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+LOGIN_URL = '/rango/login/'
 
 
 # Internationalization
